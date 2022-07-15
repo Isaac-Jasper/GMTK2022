86ba6002a3934ce7056df5d94b9eceff73dc4447
Omkar-Barkale <77471054+Omkar-Barkale@users.noreply.github.com>
2022-07-15 13:12:24 -0700

    Cleaned Up Code and Documentation
    There were inconsistencies in the last commit and for continuity, I changed some phrases and cleaned up a few brackets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //Variables holding value that determine the magintude of movment
+    //Variables holding value that determine the magnitude of movement
     [Header("Movement Modifiers")]
     [SerializeField] float minSpeed= 10f;
     [SerializeField] float maxSpeed = 15f;
@@ -16,36 +16,38 @@ public class PlayerMovement : MonoBehaviour
     public TextMeshProUGUI xOutput;
     public Rigidbody2D rb;
 
-  
-
+    //Stores magnitude of the player speed based on whether the accelerate key(Fire3 or Lshift) is pressed or not
     private float speedModifier;
 
     private void Start()
     {
+        //Sets the players speed to the base value
         speedModifier = minSpeed;
     }
 
     private void Update()
     {
+        //Gets input from the horizontal joystick/Left and Right, as well as the state of LShift
         float accelerate = Input.GetAxisRaw("Fire3");
         float currentXInput = Input.GetAxisRaw("Horizontal");
+
+
         xOutput.text = "" + speedModifier;
 
         
-
-        if( accelerate > 0 && currentXInput != 0)
-        {
-            
+        //Check if the player is moving and if the Lshift key is down, then lerps the speed modifier of the player to its max, otherwise, lerp the speed modifier to its min.
+        if( accelerate > 0 && currentXInput != 0){
             speedModifier = Mathf.Lerp(speedModifier, maxSpeed, 1000);
             print(speedModifier);
         }
-        else
-        {
+        else{
             speedModifier = Mathf.Lerp(speedModifier, minSpeed, 1000);
         }
 
+        //Make sure the speed to be applied to the player does not exceed the max speed.
         float movement = Mathf.Clamp(speedModifier * currentXInput, -1f * maxSpeed, maxSpeed);
 
+        //Apply the speed to the x component of the player, preserving the y velocity
         rb.velocity = new Vector2(movement, rb.velocity.y);
     }
 }
