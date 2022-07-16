5f581349753073caacbecd0e8cf8752814104c11
Isaac Jasper <60370374+Isaac-Jasper@users.noreply.github.com>
2022-07-15 23:22:11 -0700

    Created Rounds system
    -updated flying enemy to be better
    DiceRoll.cs:
    -added documentation
    
    FlyingEnemyAI.cs:
    -added documentation
    
    GeneralAI.cs:
    -added documentation
    -added knockback logic
    
    CustomGravity.cs:
    -added documentation
    
    GameController.cs:
    -added documentation
    -removed unused testing values
    
    -added RoundLogic.cs which handles the logic on rounds
    -this script is a singleton and can be accessed from anywere as long as only one of it exist

--- Assets/Scripts/GameScripts/GameController.cs
+++ Assets/Scripts/GameScripts/GameController.cs
@@ -4,22 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public List<int> numbers;
-    public GameObject enemy;
-    public GameObject dice;
-    public Camera Cam3D;
+    [SerializeField]
+    private int roundCount;
+
     private void Start() {
-        Cam3D.gameObject.SetActive(true);
-    }
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) enemy.GetComponent<GeneralAI>().Hit(dice); //for testing
-        if (Input.GetKey(KeyCode.X)) average(); //for testing
-    }
-    public void average() {//for testing
-        int total = 0;
-        for (int i = 0; i < numbers.Count; i++) {
-            total += numbers[i];
-        }
-        Debug.Log("average roll is: " + (float) total / numbers.Count);
+        RoundLogic.rl.startRound(roundCount); //starts round, will have trigger in future
     }
 }
